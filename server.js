520031fffabaa8f4cd8c22a858bae2c1d3aa4160
Anderson Jr <140277030+andersonjr667@users.noreply.github.com>
2025-06-04 14:19:07 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware do proxy
 app.use('/', createProxyMiddleware({
-  target: 'https://674c-2804-1b3-6147-29cd-7415-df8c-eb77-c92b.ngrok-free.app', // seu link do ngrok aqui
+  target: 'https://fefe-177-39-9-218.ngrok-free.app', // seu link do ngrok aqui
   changeOrigin: true,
   secure: false,
 }));
